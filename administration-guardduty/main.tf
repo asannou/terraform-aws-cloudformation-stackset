0a4b4e1bc9216be31cb83b27b62bc24df8e06bd3
asannou <hataori@gmail.com>
2020-03-06 13:33:59 +0900

    workaround for error putting S3 policy
    https://github.com/terraform-providers/terraform-provider-aws/issues/7628#issuecomment-469825984

--- administration-guardduty/main.tf
+++ administration-guardduty/main.tf
@@ -20,8 +20,9 @@ resource "aws_s3_bucket_public_access_block" "block" {
 }
 
 resource "aws_s3_bucket_policy" "policy" {
-  bucket = aws_s3_bucket.bucket.id
-  policy = data.aws_iam_policy_document.policy.json
+  bucket     = aws_s3_bucket.bucket.id
+  policy     = data.aws_iam_policy_document.policy.json
+  depends_on = [aws_s3_bucket_public_access_block.block]
 }
 
 data "aws_iam_policy_document" "policy" {
